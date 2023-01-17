3d0c97793eafdf5951db7015c521555b6d6231f7
Bo Latt <bolattny@gmail.com>
2023-01-17 04:50:15 -0500

    refactor event listeners

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,49 @@
 const tooltiptext = document.querySelector("#myTooltip");
 const dropZone = document.getElementById("drop-zone");
 const uploadBtn = document.getElementById("upload-btn");
+const inputEl = document.getElementById("input");
 
 function handleClick() {
   navigator.clipboard.writeText("<%=url%>");
   tooltiptext.innerText = "Copied!";
+}
+
+function validateFormat(file) {
+  console.log(file.type);
+  const validTypes = ["image/jpeg", "image/png", "image/gif"];
+  return validTypes.includes(file.type);
+}
+
+function createForm(file) {
+  let formData = new FormData();
+  formData.append("image", file);
+  return formData;
+}
+
+function uploadImage(formData) {
+  fetch("/", {
+    method: "POST",
+    body: formData,
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.url);
+      window.location.href = data.url;
+    })
+    .catch((err) => console.log(err));
+}
+
+function handleUpload(e) {
+  e.preventDefault();
+  const file = e.dataTransfer?.files[0] || e.target.files[0];
+  console.log(file);
+  const isValid = validateFormat(file);
+  if (isValid) {
+    const formData = createForm(file);
+    uploadImage(formData);
+  } else {
+    alert("not valid format");
+  }
 }
 
 dropZone?.addEventListener("dragover", (e) => {
@@ -16,38 +55,6 @@ dropZone?.addEventListener("dragleave", () => {
   console.log("drag leave ");
 });
 
-dropZone?.addEventListener("drop", (event) => {
-  event.preventDefault();
+inputEl.addEventListener("change", (e) => handleUpload(e));
 
-  const file = event.dataTransfer.files[0];
-  console.log(file);
-
-  const validTypes = ["image/jpeg", "image/png", "image/gif"];
-  if (validTypes.includes(file.type)) {
-    let formData = new FormData();
-    formData.append("image", file);
-    formData.append("isUsingFetch", true);
-    for (var pair of formData.entries()) {
-      console.log(pair[0] + ", " + pair[1]);
-    }
-
-    fetch("/", {
-      method: "POST",
-      body: formData,
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data.url);
-        window.location.href = data.url;
-      })
-      .catch((err) => console.log(err));
-  } else {
-    alert("invavlid form data");
-  }
-});
-
-function validateFormat(file) {
-  console.log(file.type);
-  if (validTypes.includes(file.type)) {
-  }
-}
+dropZone?.addEventListener("drop", (e) => handleUpload(e));
